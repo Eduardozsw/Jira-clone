b32c5f8001f52eea944dc51c0933802d6257d202
Eduardo <dudusoutomaior2006@gmail.com>
2024-11-21 23:25:02 -0300

    use login and register

--- src/features/auth/api/use-register.ts
+++ src/features/auth/api/use-register.ts
@@ -2,17 +2,17 @@ import { useMutation } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
 import{client} from "@/lib/rpc"
 
-type ResponseType = InferResponseType<typeof client.api.auth.login["$post"]>
-type RequestType =InferRequestType<typeof client.api.auth.login["$post"]>
+type ResponseType = InferResponseType<typeof client.api.auth.register["$post"]>
+type RequestType =InferRequestType<typeof client.api.auth.register["$post"]>
 
-export const useLogin = () =>{
-    const mutation= useMutation<
+export const useRegister = () =>{
+    const mutation= useMutation<    
     ResponseType,
     Error,
     RequestType
     >({
         mutationFn: async ({json,}) => {
-            const response = await client.api.auth.login["$post"]({ json });
+            const response = await client.api.auth.register["$post"]({ json });
             return await response.json()
         }
     })
