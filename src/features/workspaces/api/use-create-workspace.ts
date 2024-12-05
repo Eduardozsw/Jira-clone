c7e53c044d3b0fb87f8a1d802664e2ba365e446a
Eduardo <dudusoutomaior2006@gmail.com>
2024-12-04 22:44:57 -0300

    features update

--- src/features/workspaces/api/use-create-workspace.ts
+++ src/features/workspaces/api/use-create-workspace.ts
@@ -1,7 +1,6 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
 import { client } from "@/lib/rpc"
-import { useRouter } from "next/navigation";
 
 type ResponseType = InferResponseType<typeof client.api.workspaces["$post"]>
 type RequestType = InferRequestType<typeof client.api.workspaces["$post"]>
