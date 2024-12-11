329326652ac9f2f4974af716ed1b756a2203de56
Eduardo <dudusoutomaior2006@gmail.com>
2024-12-10 23:47:15 -0300

    layout update

--- src/app/(standalone)/layout.tsx
+++ src/app/(standalone)/layout.tsx
@@ -1,3 +1,5 @@
+import { UserButton } from "@/features/auth/components/user-button";
+import Image from "next/image";
 import Link from "next/link";
 
 interface StandloneLayoutProps {
@@ -9,7 +11,10 @@ const StandloneLayout = ({children}: StandloneLayoutProps) => {
         <main className="bg-neutral-100 min-h-screen">
             <div className="mx-auto max-w-screen-2xl p-4">
                 <nav className="flex justify-between items-center h-[73px]">
-                    <Link></Link>
+                    <Link href="/">
+                        <Image src="/logo.svg" alt="logo" height={56} width={152}/>
+                    </Link>
+                    <UserButton />
                 </nav>
                 <div className="flex flex-col items-center justify-center py-4">
                     {children}
