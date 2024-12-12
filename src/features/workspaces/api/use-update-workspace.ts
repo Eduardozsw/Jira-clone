423500db6caecc6f9178bfbc137da850d8aa620a
Eduardo <dudusoutomaior2006@gmail.com>
2024-12-12 16:30:09 -0300

    use update

---
+++ src/features/workspaces/api/use-update-workspace.ts
@@ -0,0 +1,35 @@
+import { toast } from "sonner";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+import { client } from "@/lib/rpc"
+
+type ResponseType = InferResponseType<typeof client.api.workspaces[":workspaecId"]["$patch"], 200>
+type RequestType = InferRequestType<typeof client.api.workspaces[":workspaecId"]["$patch"]>
+
+export const useUpdateeWorkspace = () => {
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation<
+        ResponseType,
+        Error,
+        RequestType
+    >({
+        mutationFn: async ({ form, param }) => {
+            const response = await client.api.workspaces[":workspaecId"]["$patch"]({ form, param });
+            if (!response.ok) {
+                throw new Error("Failed to update workspace");
+            }
+
+            return await response.json();
+        },
+        onSuccess: ({ data }) => {
+            toast.success("Workspace updated");
+            queryClient.invalidateQueries({ queryKey: ["workspaces"] })
+            queryClient.invalidateQueries({ queryKey: ["workspaces", data.$id] })
+        },
+        onError: () => {
+            toast.error("Failed to update workspace");
+        }
+    })
+    return mutation;
+}
