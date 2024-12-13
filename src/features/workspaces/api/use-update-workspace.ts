41b0f774acb9418d4442ce0a9b4147024ba6348f
Eduardo <dudusoutomaior2006@gmail.com>
2024-12-13 20:54:16 -0300

    code review

--- src/features/workspaces/api/use-update-workspace.ts
+++ src/features/workspaces/api/use-update-workspace.ts
@@ -1,12 +1,13 @@
 import { toast } from "sonner";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
+
 import { client } from "@/lib/rpc"
 
-type ResponseType = InferResponseType<typeof client.api.workspaces[":workspaecId"]["$patch"], 200>
-type RequestType = InferRequestType<typeof client.api.workspaces[":workspaecId"]["$patch"]>
+type ResponseType = InferResponseType<typeof client.api.workspaces[":workspaceId"]["$patch"], 200>
+type RequestType = InferRequestType<typeof client.api.workspaces[":workspaceId"]["$patch"]>
 
-export const useUpdateeWorkspace = () => {
+export const useUpdateWorkspace = () => {
     const queryClient = useQueryClient();
 
     const mutation = useMutation<
@@ -15,17 +16,17 @@ export const useUpdateeWorkspace = () => {
         RequestType
     >({
         mutationFn: async ({ form, param }) => {
-            const response = await client.api.workspaces[":workspaecId"]["$patch"]({ form, param });
-            if (!response.ok) {
+            const response = await client.api.workspaces[":workspaceId"]["$patch"]({ form, param });
+            if(!response.ok) {
                 throw new Error("Failed to update workspace");
             }
-
+            
             return await response.json();
         },
         onSuccess: ({ data }) => {
             toast.success("Workspace updated");
             queryClient.invalidateQueries({ queryKey: ["workspaces"] })
-            queryClient.invalidateQueries({ queryKey: ["workspaces", data.$id] })
+            queryClient.invalidateQueries({ queryKey: ["workspace", data.$id] })
         },
         onError: () => {
             toast.error("Failed to update workspace");
