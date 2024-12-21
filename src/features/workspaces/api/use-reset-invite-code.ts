abf472c55492a1d1ee0e65ebcaaa988d10f1f233
Eduardo <dudusoutomaior2006@gmail.com>
2024-12-21 00:21:41 -0300

    code review

--- src/features/workspaces/api/use-reset-invite-code.ts
+++ src/features/workspaces/api/use-reset-invite-code.ts
@@ -1,12 +1,15 @@
 import { toast } from "sonner";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 import { InferRequestType, InferResponseType } from "hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
 import { client } from "@/lib/rpc"
 
 type ResponseType = InferResponseType<typeof client.api.workspaces[":workspaceId"]["reset-invite-code"]["$post"], 200>
 type RequestType = InferRequestType<typeof client.api.workspaces[":workspaceId"]["reset-invite-code"]["$post"]>
 
 export const useResetInviteCode = () => {
+    const router = useRouter();
     const queryClient = useQueryClient();
 
     const mutation = useMutation<
@@ -16,14 +19,16 @@ export const useResetInviteCode = () => {
     >({
         mutationFn: async ({ param }) => {
             const response = await client.api.workspaces[":workspaceId"]["reset-invite-code"]["$post"]({ param });
-            if(!response.ok) {
+            if (!response.ok) {
                 throw new Error("Failed to reset invite code");
             }
-            
+
             return await response.json();
         },
         onSuccess: ({ data }) => {
             toast.success("Invite code reseted");
+            
+            router.refresh()
             queryClient.invalidateQueries({ queryKey: ["workspaces"] })
             queryClient.invalidateQueries({ queryKey: ["workspace", data.$id] })
         },
