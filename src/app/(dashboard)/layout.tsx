dfc8dd46dbeb35026614fdab8cf62f4a5c9b4a2f
Eduardo <dudusoutomaior2006@gmail.com>
2024-12-25 23:45:16 -0300

    task modal

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import { CreateTaskModal } from "@/features/tasks/components/create-task-modal";
 import { CreateProjectModal } from "@/features/projects/components/create-project-modal";
 import { CreateWorkspaceModal } from "@/features/workspaces/components/create-workspace-modal";
 
@@ -13,9 +14,10 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
         <div className="min-h-screen">
             <CreateWorkspaceModal />
             <CreateProjectModal />
+            <CreateTaskModal />
             <div className="flex w-full h-full">
                 <div className="fixed left-0 top-0 hidden lg:block lg:w-[264px] h-full overflow-y-auto">
-                <Sidebar/>
+                    <Sidebar />
                 </div>
                 <div className="lg:pl-[264px] w-full">
                     <div className="mx-auto max-w-screen-2xl h-full">
