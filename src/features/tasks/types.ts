fdefccfd7bc4adfaecb598bea2695688e5dc8e2e
Eduardo <dudusoutomaior2006@gmail.com>
2025-01-02 23:21:00 -0300

    task types search

--- src/features/tasks/types.ts
+++ src/features/tasks/types.ts
@@ -1,8 +1,18 @@
-export enum TaskStatus {
+import { Models } from "node-appwrite";
 
+export enum TaskStatus {
     BACKLOG = "BACKLOG",
     TODO = "TODO",
-    IN_PROGESS = "IN_PROGESS",
+    IN_PROGRESS = "IN_PROGRESS",
     IN_REVIEW = "IN_REVIEW",
     DONE = "DONE",
+};
+
+export type Task = Models.Document & {
+    name: string;
+    status: TaskStatus;
+    assigneeId: string;
+    projectId: string;
+    position: number;
+    dueDate: string;
 }
