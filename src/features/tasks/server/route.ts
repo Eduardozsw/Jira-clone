d5bc8128ce127d1cd0b76161218748b506873e17
Eduardo <dudusoutomaior2006@gmail.com>
2025-01-04 21:25:02 -0300

    delete and patch method

--- src/features/tasks/server/route.ts
+++ src/features/tasks/server/route.ts
@@ -14,6 +14,39 @@ import { Task, TaskStatus } from "../types";
 import { createTaskSchema } from "../schemas";
 
 const app = new Hono()
+  .delete(
+    "/:taskId",
+    sessionMiddleware,
+    async (c) => {
+      const user = c.get("user");
+      const databases = c.get("databases");
+      const { taskId } = c.req.param();
+
+      const task = await databases.getDocument<Task>(
+        DATABASE_ID,
+        TASKS_ID,
+        taskId
+      )
+
+      const member = await getMember({
+        databases,
+        workspaceId: task.workspaceId,
+        userId: user.$id
+      });
+
+      if (!member) {
+        return c.json({ error: "Unauthorized" }, 401)
+      }
+
+      await databases.deleteDocument(
+        DATABASE_ID,
+        TASKS_ID,
+        taskId
+      )
+
+      return c.json({ data: { $id: task.$id } })
+    }
+  )
   .get(
     "/",
     sessionMiddleware,
@@ -98,7 +131,7 @@ const app = new Hono()
         assigneeIds.length > 0 ? [Query.contains("$id", assigneeIds)] : []
       )
       const assignees = await Promise.all(
-        members.documents.map(async(member) => {
+        members.documents.map(async (member) => {
           const user = await users.get(member.userId)
 
           return {
@@ -136,6 +169,66 @@ const app = new Hono()
   )
 
   .post(
+    "/",
+    sessionMiddleware,
+    zValidator("json", createTaskSchema),
+    async (c) => {
+      const user = c.get("user")
+      const databases = c.get("databases")
+      const {
+        name,
+        status,
+        workspaceId,
+        projectId,
+        dueDate,
+        assigneeId
+      } = c.req.valid("json")
+
+      const member = await getMember({
+        databases,
+        workspaceId,
+        userId: user.$id
+      })
+
+      if (!member) {
+        return c.json({ error: "Unauthorized" }, 401)
+      }
+
+      const highestPositionTask = await databases.listDocuments(
+        DATABASE_ID,
+        TASKS_ID,
+        [
+          Query.equal("status", status),
+          Query.equal("workspaceId", workspaceId),
+          Query.orderAsc("position"),
+          Query.limit(1)
+        ]
+      )
+
+      const newPosition =
+        highestPositionTask.documents.length > 0
+          ? highestPositionTask.documents[0].position + 1000
+          : 1000
+
+      const task = await databases.createDocument(
+        DATABASE_ID,
+        TASKS_ID,
+        ID.unique(),
+        {
+          name,
+          status,
+          workspaceId,
+          projectId,
+          dueDate,
+          assigneeId,
+          position: newPosition
+        }
+      )
+
+      return c.json({ data: task })
+    }
+  )
+  .patch(
     "/",
     sessionMiddleware,
     zValidator("json", createTaskSchema),
