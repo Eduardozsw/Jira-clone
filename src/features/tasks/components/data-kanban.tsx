99b964e51b9c75f5c0e5ce16be080975d2043646
Marcus-Vin <dudusoutomaior2006@gmail.com>
2025-01-08 10:27:22 -0300

    Data Kanban

--- src/features/tasks/components/data-kanban.tsx
+++ src/features/tasks/components/data-kanban.tsx
@@ -12,16 +12,24 @@ import { Task, TaskStatus } from "../types";
 const boards: TaskStatus[] = [
     TaskStatus.BACKLOG,
     TaskStatus.TODO,
-    TaskStatus.DONE,
     TaskStatus.IN_PROGRESS,
     TaskStatus.IN_REVIEW,
+    TaskStatus.DONE,
 ]
+
+type TasksState = {
+    [key in TaskStatus]: Task[]
+}
 
 interface DataKanbanProps {
     data: Task[];
 }
 
 export const DataKanban = ({ data }: DataKanbanProps) => {
+    const [tasks, setTasks] = useState<TasksState>(() => {
+        const initialTasks: TasksState = {}
+    })
+
     return (
         <div>
             Data Kanban
