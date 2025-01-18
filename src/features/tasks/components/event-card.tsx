a25a1045a8012591b3cac8f3fedaa76c3247ffcb
Eduardo <dudusoutomaior2006@gmail.com>
2025-01-18 19:56:23 -0300

    event card

---
+++ src/features/tasks/components/event-card.tsx
@@ -0,0 +1,41 @@
+import { Project } from "@/features/projects/types";
+
+import { cn } from "@/lib/utils";
+
+import { TaskStatus } from "../types";
+
+
+interface EventCardProps {
+  title: string;
+  assignee: undefined;
+  project: Project;
+  status: TaskStatus;
+  id: string;
+};
+
+const statusColorMap: Record<TaskStatus, string> = {
+  [TaskStatus.BACKLOG]: "border-l-pink-500",
+  [TaskStatus.TODO]: "border-l-red-500",
+  [TaskStatus.IN_PROGRESS]: "border-l-yellow-500",
+  [TaskStatus.IN_REVIEW]: "border-l-blue-500",
+  [TaskStatus.DONE]: "border-l-emerald-500",
+} 
+
+export const EventCard = ({
+  title,
+  assignee,
+  project,
+  status,
+  id,
+} : EventCardProps) => {
+  return (
+    <div className="px-2">
+      <div className={cn(
+        "p-1.5 text-xs bg-white text-primary border rounded-md border-l-4 flex flex-col gap-y-1.5 cursor-pointer hover:opacity-75 transition",
+        statusColorMap[status]
+      )}>
+        <p>{title}</p>
+      </div>
+    </div>
+  )
+}
