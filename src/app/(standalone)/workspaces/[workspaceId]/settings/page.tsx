1a1c24adb2decef11144f0dd28f244fcb6a9a987
Eduardo <dudusoutomaior2006@gmail.com>
2025-01-25 23:57:15 -0300

    bug fix

--- src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
@@ -2,13 +2,15 @@ import { redirect } from "next/navigation";
 
 import { getCurrent } from "@/features/auth/queries";
 
+import { WorkspaceIdSettingsClient } from "./client";
+
 const WorkspaceIdSettingsPage = async () => {
     const user = await getCurrent();
     if (!user) redirect("/sign-in");
 
 
     return (
-        <WorkspaceIdSettingsPage />
+        <WorkspaceIdSettingsClient />
     )
 }
 
