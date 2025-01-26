241a71358d106ebc247da4c387f3610a85beb890
Eduardo <dudusoutomaior2006@gmail.com>
2025-01-26 00:10:02 -0300

    code review

--- src/app/(standalone)/workspaces/[workspaceId]/join/[inviteCode]/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/join/[inviteCode]/page.tsx
@@ -1,25 +1,11 @@
-import { getCurrent } from "@/features/auth/queries";
-import { JoinWorkspaceForm } from "@/features/workspaces/components/join-workspace-form";
-import { getWorkspaceInfo } from "@/features/workspaces/queries";
 import { redirect } from "next/navigation";
 
-interface WorkspaceIdJoinPageProps {
-  params: {
-    workspaceId: string;
-  }
-}
+import { getCurrent } from "@/features/auth/queries";
+import { JoinWorkspaceForm } from "@/features/workspaces/components/join-workspace-form";
 
-const WorkspaceIdJoinPage = async ({ params, }: WorkspaceIdJoinPageProps) => {
+const WorkspaceIdJoinPage = async () => {
   const user = await getCurrent()
   if (!user) redirect("/sign-in")
-
-  const initialValues = await getWorkspaceInfo({
-    workspaceId: params.workspaceId
-  })
-
-  if (!initialValues) {
-    redirect("/")
-  }
 
   return (
     <div className="w-full lg:max-w-xl">
