405cdaa57bbfc9e2afa2153dd16737659f725045
Eduardo <dudusoutomaior2006@gmail.com>
2025-01-28 11:43:49 -0300

    return analytics

--- src/features/projects/server/route.ts
+++ src/features/projects/server/route.ts
@@ -279,6 +279,31 @@ const app = new Hono()
       const taskCount = thisMonthTasks.total
       const taskDifference = taskCount - lastMonthTasks.total
 
+      const thisMonthIncompleteTasks = await databases.listDocuments(
+        DATABASE_ID,
+        TASKS_ID,
+        [
+          Query.equal("projectId", projectId),
+          Query.notEqual("status", TaskStatus.DONE),
+          Query.greaterThanEqual("$createdAt", thisMonthStart.toISOString()),
+          Query.lessThanEqual("$createdAt", thisMonthEnd.toISOString()),
+        ]
+      )
+
+      const lastMonthIncompleteTasks = await databases.listDocuments(
+        DATABASE_ID,
+        TASKS_ID,
+        [
+          Query.equal("projectId", projectId),
+          Query.notEqual("status", TaskStatus.DONE),
+          Query.greaterThanEqual("$createdAt", lastMonthStart.toISOString()),
+          Query.lessThanEqual("$createdAt", lastMonthEnd.toISOString()),
+        ]
+      )
+
+      const incompleteTaskCount = thisMonthIncompleteTasks.total
+      const incompleteTaskDifference = incompleteTaskCount - lastMonthIncompleteTasks.total
+
       const thisMonthAssignedTasks = await databases.listDocuments(
         DATABASE_ID,
         TASKS_ID,
@@ -304,57 +329,72 @@ const app = new Hono()
       const assignedTaskCount = thisMonthAssignedTasks.total
       const assignedTaskDifference = assignedTaskCount - lastMonthAssignedTasks.total;
 
-      const thisMonthIncompleteTasks = await databases.listDocuments(
+      const thisMonthCompletedTasks = await databases.listDocuments(
         DATABASE_ID,
         TASKS_ID,
         [
           Query.equal("projectId", projectId),
-          Query.notEqual("status", TaskStatus.DONE),
+          Query.equal("status", TaskStatus.DONE),
           Query.greaterThanEqual("$createdAt", thisMonthStart.toISOString()),
           Query.lessThanEqual("$createdAt", thisMonthEnd.toISOString()),
         ]
       )
 
-      const lastMonthIncompleteTasks = await databases.listDocuments(
+      const lastMonthCompletedTasks = await databases.listDocuments(
         DATABASE_ID,
         TASKS_ID,
         [
           Query.equal("projectId", projectId),
-          Query.notEqual("status", TaskStatus.DONE),
+          Query.equal("status", TaskStatus.DONE),
           Query.greaterThanEqual("$createdAt", lastMonthStart.toISOString()),
           Query.lessThanEqual("$createdAt", lastMonthEnd.toISOString()),
         ]
       )
 
-      const incompleteTaskCount = thisMonthIncompleteTasks.total
-      const incompleteTaskDifference = incompleteTaskCount - lastMonthIncompleteTasks.total
-      
-      const thisMonthIncompleteTasks = await databases.listDocuments(
+      const completedTaskCount = thisMonthCompletedTasks.total
+      const completedTaskDifference = completedTaskCount - lastMonthCompletedTasks.total
+
+      const thisMonthOverdueTasks = await databases.listDocuments(
         DATABASE_ID,
         TASKS_ID,
         [
           Query.equal("projectId", projectId),
           Query.notEqual("status", TaskStatus.DONE),
+          Query.lessThan("dueDate", now.toISOString()),
           Query.greaterThanEqual("$createdAt", thisMonthStart.toISOString()),
           Query.lessThanEqual("$createdAt", thisMonthEnd.toISOString()),
         ]
       )
 
-      const lastMonthIncompleteTasks = await databases.listDocuments(
+      const lastMonthOverdueTasks = await databases.listDocuments(
         DATABASE_ID,
         TASKS_ID,
         [
           Query.equal("projectId", projectId),
           Query.notEqual("status", TaskStatus.DONE),
+          Query.lessThan("dueDate", now.toISOString()),
           Query.greaterThanEqual("$createdAt", lastMonthStart.toISOString()),
           Query.lessThanEqual("$createdAt", lastMonthEnd.toISOString()),
         ]
       )
 
-      const incompleteTaskCount = thisMonthIncompleteTasks.total
-      const incompleteTaskDifference = incompleteTaskCount - lastMonthIncompleteTasks.total
+      const overdueTaskCount = thisMonthOverdueTasks.total
+      const overdueTaskDifference = overdueTaskCount - lastMonthOverdueTasks.total
 
-
+      return c.json({
+        data: {
+          taskCount,
+          taskDifference,
+          assignedTaskCount,
+          assignedTaskDifference,
+          completedTaskCount,
+          completedTaskDifference,
+          incompleteTaskCount,
+          incompleteTaskDifference,
+          overdueTaskCount,
+          overdueTaskDifference,
+        }
+      })
     }
   )
 export default app;
